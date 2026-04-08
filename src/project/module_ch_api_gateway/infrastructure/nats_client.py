671706695405fc02dbd21c5dead101dd029fc5c6
Maksim <i@mpozdnjakov.ru>
2026-04-08 12:18:06 +0300

    Реализована 2 задача для работы с ЛК.

--- src/project/module_ch_api_gateway/infrastructure/nats_client.py
+++ src/project/module_ch_api_gateway/infrastructure/nats_client.py
@@ -22,18 +22,18 @@ class NatsInfrastructure:
             raise RuntimeError("NATS is not connected")
         await self.js.publish(subject, json.dumps(data).encode())
 
-        async def request(self,
-                          subject: str,
-                          data: dict,
-                          timeout: float) -> dict:  # <- добавить
-            if not self.nc or not self.nc.is_connected:
-                raise RuntimeError("NATS is not connected")
-            msg = await self.nc.request(
-                subject,
-                json.dumps(data).encode(),
-                timeout=timeout,
-            )
-            return json.loads(msg.data.decode())
+    async def request(self,
+                      subject: str,
+                      data: dict,
+                      timeout: float) -> dict:  # <- добавить
+        if not self.nc or not self.nc.is_connected:
+            raise RuntimeError("NATS is not connected")
+        msg = await self.nc.request(
+            subject,
+            json.dumps(data).encode(),
+            timeout=timeout,
+        )
+        return json.loads(msg.data.decode())
 
     async def close(self):
         if self.nc:
